a51d3d26b4405839566c180baebe04b1c9e76419
Fredrik Tolf <fredrik@dolda2000.com>
2014-09-14 05:48:32 +0200

    Added a distutils script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name = "pycfml",
+      version = "0.1",
+      description = "Java classfile manipulation library",
+      author = "Fredrik Tolf",
+      author_email = "fredrik@dolda2000.com",
+      packages = ["classfile"],
+      license = "GPL-3")
